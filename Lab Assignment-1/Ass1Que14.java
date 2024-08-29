43e2fb51bfb78ac292ed50984bb2949a3f1775f7
pratham <prathamrathod1812@gmail.com>
2024-08-29 22:53:31 +0530

    Ass1Que14 file added

---
+++ Lab Assignment-1/Ass1Que14.java
@@ -0,0 +1,5 @@
+// 14. Write a program to access the public, protected, private and default member of a class in the same package and also in different package. 
+
+public class Ass1Que14 {
+    
+}
